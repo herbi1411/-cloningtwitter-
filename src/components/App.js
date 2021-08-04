97fd4ca98b3bb2efb26c4a4e35d41e0eb0d9ea47
YoonMinSoo <starex2052@daum.net>
2021-08-04 15:57:28 +0900

    #2.0 using filebase auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
 import AppRouter from "./Router"
+import {authService} from "fbase";
 function App() {
+//  console.log(authService.currentUser);
+  const [isLoggedin, setIsLoggedIn] = useState(true);
   return (
-    <div className="App">
-      <AppRouter/>
-    </div>
+      <>
+      <AppRouter isLoggedin={isLoggedin}/>
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+      </>
+
   );
 }
 
