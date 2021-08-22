8b64471d11cb2d003045b031e0891f4fad741625
YoonMinSoo <starex2052@daum.net>
2021-08-22 16:04:17 +0900

    #3.4 Delete and Update part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({userObj})=> {
     const [nweet, setNweet] = useState("");
@@ -46,9 +47,9 @@ const Home = ({userObj})=> {
         <input type="submit" value="Nweet"/>
     </form>
     <div>
-        {nweets.map(nweet => <div key={nweet.id}>
-            <h4>{nweet.text}</h4> 
-            </div>)}
+        {nweets.map( nweet => (
+            <Nweet key={nweet.id} nweetObj = {nweet} isOwner = {nweet.creatorId === userObj.uid}/>
+        ))}
     </div>
 </div>
 }
