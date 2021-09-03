7095c735d6ba3a125419a718e5c0f29e0f46f7d4
YoonMinSoo <starex2052@daum.net>
2021-09-03 14:16:56 +0900

    #5.0 Get My Own Nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -15,7 +15,7 @@ const AppRouter = ({isLoggedin, userObj})=>{
                 <Home userObj = {userObj}/>
             </Route>
             <Route exact path = "/profile">
-                <Profile/>
+                <Profile userObj = {userObj}/>
             </Route>
             </>
              : <Route exact path = "/"><Auth /></Route>}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,18 @@
-import React from "react"
-import { authService } from "fbase";
+import React, { useEffect } from "react"
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = ()=>{
+const Profile = ({userObj})=>{
     const history = useHistory();
+
+    useEffect(()=>{
+        getMyNweets();
+    },[]);
+
+    const getMyNweets = async() => {
+        const nweets = await dbService.collection("nweets").where("creatorid","==",userObj.uid).orderBy("createdAt","asc").get();
+        console.log(nweets.docs.map((doc)=>doc.data()));
+    }
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
